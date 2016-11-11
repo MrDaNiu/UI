3298f084a2f03eff3229dd819117c0dd1f65f20a
fusen <123070687@qq.com>
2016-11-11 15:08:39 +0800

    代码精简优化

--- roll-message-bar/rollMsgBar.js
+++ roll-message-bar/rollMsgBar.js
@@ -1,49 +1,31 @@
 $.fn.extend({
-	rollMsgBar: function(showMsgCounts) {
-		if(!showMsgCounts){
-			showMsgCounts = 5;
-		}
+	rollMsgBar: function() {
 		var self = $(this);
 		var ul = $(self.find('ul').first());
-		var infoCount = ul.find("li").length;
-		if(infoCount > showMsgCounts) {
-			var clone = $(ul.find("li")).clone();
-			ul.append(clone);
-		}
-		var lineheight = $(ul.find("li")).height() + 1;
-		var i = 0
 		var init = function() {
-			if(infoCount > showMsgCounts) {
-				var t = setInterval(function() {
-					if(infoCount > i) {
-						i++;
-					} else {
-						ul.css('top', 0);
-						i = 1;
-					}
+			var t = setInterval(function() {
+				move()
+			}, 2000)
+			self.hover(function() {
+				clearInterval(t);
+			}, function() {
+				t = setInterval(function() {
 					move()
 				}, 2000)
-				self.hover(function() {
-					clearInterval(t);
-				}, function() {
-					t = setInterval(function() {
-						if(infoCount > i) {
-							i++;
-						} else {
-							ul.css('top', 0);
-							i = 1;
-						}
-						move()
-					}, 2000)
-				})
-			}
+			})
 		}
 		var move = function() {
 			ul.stop().animate({
-				top: -i * lineheight
-			}, 1000)
+				top: -1 * $(ul.find("li")).height()
+			}, 1000,function(){
+				$(ul.find("li").first()).appendTo(ul);
+				ul.css("top",0);
+			})
 		}
-		init();
+		
+		if(self.height() < ul.height()){
+			init();
+		}
 	}
 })
 //$(".scrollbar").rollMsgBar(7);
